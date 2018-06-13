89ba67277fdec23cc013654f8b763b313b66598c
Noah Huber-Feely <nhuberfeely@gmail.com>
2018-06-12 22:04:49 -0400

    correct naming

--- sync/getMyFitnessPal.py
+++ sync/getMyFitnessPal.py
@@ -1,3 +1,8 @@
+import sys
+from os import path
+sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
+import utils.loadConfig
+
 import myfitnesspal
 from utils import loadConfig
 
@@ -6,6 +11,7 @@ config = loadConfig.getConfig()
 user_name = config['keys']['myfitnesspal']['username']
 client = myfitnesspal.Client(user_name)
 
+current = (0
 lastSync = (2013, 3, 2)
 while current <= today:
   day = client.get_date(current.year, current.month, current.day)
