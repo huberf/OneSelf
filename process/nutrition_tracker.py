4e1a8c25f26dd7e50159cf96deb6c357fcb35fda
Noah Huber-Feely <nhuberfeely@gmail.com>
2018-07-17 18:36:23 -0400

    add recent vs historical analysis

--- process/nutrition_tracker.py
+++ process/nutrition_tracker.py
@@ -18,6 +18,7 @@ except:
     sys.exit()
 
 BASELINE_METRICS = {
+    'calories': 2200,
     'carbohydrates': 300,
     'fat': 65,
     'protein': 50,
@@ -42,6 +43,30 @@ def current_health(data):
     return
 
 def recent_changes(data):
+    day_data = data['data']
+    top = len(day_data)
+    bottom = len(day_data) - 8
+    if bottom < 0:
+        bottom = 0
+    last_week = day_data[bottom:top]
+    top = bottom
+    bottom = top - 7*3
+    if bottom < 0:
+        bottom = 0
+    three_weeks_before = day_data[bottom:top]
+    recent_norms = {}
+    historical_norms = {}
+    for i in BASELINE_METRICS.keys():
+        recent_norms[i] = 0
+        historical_norms[i] = 0
+    for i in last_seven_days:
+        for j in recent_norms['keys']:
+            recent_norms[j] += i['day']['totals'][j]
+    for i in three_weeks_before:
+        for j in historical_norms['keys']:
+            historical_norms[j] += i['day']['totals'][j]
+    for i in recent_norms.keys():
+        print('Recently you had {0} of {1} vs. historically having {2}'.format(recent_norms[i]/7.0, i, historical_norms[i]/(7.0*3)))
     return
 
 def longterm_health(data):
