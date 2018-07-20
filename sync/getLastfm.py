f71c6e2bb097695122680be6d45c215506f71861
Noah Huber-Feely <nhuberfeely@gmail.com>
2018-07-20 19:57:05 -0400

    add v1 of lastfm syncer

---
+++ sync/getLastfm.py
@@ -0,0 +1,29 @@
+import json
+import sys
+from os import path
+sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
+import utils.loadConfig
+
+from lastpy import extras
+
+user_name = config['keys']['lastfm']['username']
+
+sync_back_days = 100
+
+def save(data):
+    to_save = { 'data': data, 'lastsave': str(datetime.datetime.now()) }
+    save_file = open('records/lastfm-data.json', 'w')
+    save_file.write(json.dumps(to_save, indent=4))
+    save_file.close()
+
+data = []
+
+for i in range(0, sync_back_days):
+    print('Saving day {0} of {1}'.format(i, sync_back_days))
+    back_days = sync_back_days - i
+    tracks = extras.user_daily_tracks(user_name, back_days)
+    data += tracks
+
+print('Saving data...')
+save(data)
+print('Data saved.')
