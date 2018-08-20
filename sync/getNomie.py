9d061f1847f3e4dfbcee6dfef603edac0247cda2
Noah Huber-Feely <nhuberfeely@gmail.com>
2018-08-20 15:23:34 -0500

    add beggining of support for Nomie

---
+++ sync/getNomie.py
@@ -0,0 +1,5 @@
+# This syncer will support retreival and processing of Nomie 1, 2 and 3 data
+# in the near future. For now the system solely supports using data from the
+# Nomie Dropbox backup feature..
+
+print('View instructions in the README to acquire data from a Dropbox backup.')
