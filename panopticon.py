7324fe1a51554e5b5f9751f12bda8acc6d401f04
Noah Huber-Feely <nhuberfeely@gmail.com>
2018-08-27 13:16:05 -0500

    add response cleansing

--- panopticon.py
+++ panopticon.py
@@ -12,19 +12,33 @@ reports = [
 
 responses_library = {}
 
+def show_update(updated_line):
+    print(updated_line)
+
 def analyze_report(report_name):
+    global responses_library
     text = subprocess.check_output(['python3', 'process/{name}.py'.format(name=report_name)])
     lines = text.split(b'\n')
     for i in lines:
         try:
             responses_library[i]
-        except:
-            print('{0}'.format(i))
             responses_library[i] = 0
+        except:
+            show_update(i)
+            responses_library[i] = 0
+
+def clean_old_responses():
+    global responses_library
+    for i in list(responses_library.keys()):
+        if responses_library[i] > 0:
+            del responses_library[i]
+        else:
+            responses_library[i] += 1
 
 def check_reports():
     for i in reports:
         analyze_report(i)
+    clean_old_responses()
 
 ticks = 0
 while True:
