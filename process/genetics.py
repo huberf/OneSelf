d011069c5d72a9f2b6e22bad5f52c4b54c7dd2ed
Noah Huber-Feely <nhuberfeely@gmail.com>
2018-09-01 20:38:03 -0400

    add processing of genetics file

--- process/genetics.py
+++ process/genetics.py
@@ -9,11 +9,24 @@ import utils
 
 config = loadConfig.getConfig()
 
-genetic_data = {}
+genetic_data = []
 
 try:
     genome_file = open('records/genome_data.txt')
     raw_contents = genome_file.read()
+    lines = raw_contents.split('\n')
+    for i in lines:
+        if len(i) > 0 and i[0] == '#':
+            continue # Line is a comment
+        elif len(i) > 0:
+            # 0=rsid, 1=chromosome, 2=position, 3=genotype
+            columns = i.split('\t')
+            genetic_data += [{
+                'rsid': columns[0],
+                'chromosome': columns[1],
+                'position': columns[2],
+                'genotype': columns[3]
+                }]
 except IOError as e:
     print('Make sure you\'ve setup your genetic data')
     sys.exit()
