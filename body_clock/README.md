6ca53d45475a2c8809bbdbb7f6a23680b5df2721
Noah Huber-Feely <nhuberfeely@gmail.com>
2018-09-15 20:19:36 -0400

    [body_clock] initialize body clock project

---
+++ body_clock/README.md
@@ -0,0 +1,8 @@
+# QS Body Clock
+
+The idea behind this is one can keep track of valuable metrics that occur
+at set times or time ranges throughout the day to get an at a glance view of how
+various aspects will impact your day. Visually the system will have a clock of the day and
+around its edge various events are placed such as ones highest blood pressure,
+sleep period, best time for exercise and other lifestyle and physiological
+components.
---
+++ body_clock/index.js
@@ -0,0 +1,47 @@
+//Getting all dependencies
+var express = require('express');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var app = express();
+var nodemailer = require('nodemailer');
+var bodyParser = require('body-parser');
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+//Setting up the port to listen to
+app.set('port', (process.env.PORT || 5000));
+
+//Setting up the resource directory
+app.use(express.static(__dirname + '/public'));
+
+app.use( bodyParser.urlencoded({ extended: false }));
+app.use( bodyParser.json());
+
+//Setting up cookie use
+app.use(cookieParser());
+
+//Setting up session handling
+app.use(session({secret: 's3cr3tsSh0uldB3K3pt'}));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+
+app.get('/', function(req, res) {
+	res.render('pages/index');
+});
+
+io.sockets.on('connection', function(socket) {
+  /*
+  socket.on('test', function(data) {
+    var data = [];
+    io.emit('testsend', data);
+  }
+   */
+});
+
+http.listen(app.get('port'), function() {
+  console.log('Node app is running on port ', app.get('port'));
+});
+
