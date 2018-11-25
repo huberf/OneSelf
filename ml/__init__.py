d44e9e3c147a08d8acbc4315d44ab652c193f56a
Noah Huber-Feely <nhuberfeely@gmail.com>
2018-11-24 20:11:27 -0500

    [ml] add dependencies

--- ml/__init__.py
+++ ml/__init__.py
@@ -1,3 +1,29 @@
 # This is intended to be a suite of common tools and functions
 # for the machine learning functionality of the OneSelf project
 # components.
+
+# General 3rd party library imports
+import json
+import os
+
+# Specific 3rd party library imports
+from os import listdir
+
+# Local imports
+try:
+    from utils import convo_reader
+except ModuleNotFoundError:
+    from clara.utils import convo_reader
+
+# ML specific library imports
+import nltk
+from nltk.stem.lancaster import LancasterStemmer
+stemmer = LancasterStemmer()
+
+# things we need for Tensorflow
+import numpy as np
+import tflearn
+import tensorflow as tf
+from tensorflow.python.saved_model import tag_constants
+import random
+
