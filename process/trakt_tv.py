d041c81e273602df1e0a1fd67b80938cb467e873
Noah Huber-Feely <nhuberfeely@gmail.com>
2019-01-02 09:09:05 -0500

    [trakt.tv] make extensive multiyear HTML report generation

--- process/trakt_tv.py
+++ process/trakt_tv.py
@@ -4,6 +4,7 @@ import datetime
 sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
 import utils
 import utils.loadConfig
+import generator
 
 import csv
 
@@ -38,17 +39,39 @@ def load_data():
         record_data += [record]
     return { 'records': record_data }
 
-def year_avg_watchtime(data, just_tv=False):
+def year_avg_watchtime(data, just_tv=False, year=None):
+    current_year = datetime.datetime.now().year
+    if year == None:
+        year = current_year
     total_time = 0
     current = datetime.datetime.now()
     for i in data['records']:
         if (not just_tv or i['type'] == 'episode'):
-            if i['watched_at'][0:4] == str(datetime.datetime.now().year):
+            if i['watched_at'][0:4] == str(year):
                 total_time += i['runtime']
-    days = (datetime.date.today() - datetime.date(current.year, 1, 1)).days
-    average_per_day = float(total_time) / days
+    if year == current_year:
+        days = (datetime.date.today() - datetime.date(current.year, 1, 1)).days
+    else:
+        days = 365 # Leap years exist but not a big difference
+    if days > 0:
+        average_per_day = float(total_time) / days
+    else:
+        average_per_day = 0
     hours_per_day = average_per_day/60
     return hours_per_day
+
+def avg_watchtime_all_years(data):
+    curr_year = datetime.datetime.now().year
+    to_return = {}
+    while curr_year > 2000:
+        avg_all = year_avg_watchtime(data, False, curr_year)
+        if avg_all > 0:
+            to_return[curr_year] = {
+                    'avg_all': avg_all,
+                    'avg_tv': year_avg_watchtime(data, True, curr_year)
+                    }
+        curr_year -= 1
+    return to_return
 
 def top_shows(data, mode='watchtime'):
     show_to_weight = {}
@@ -64,9 +87,26 @@ def top_shows(data, mode='watchtime'):
     return sorted_list
 
 data = load_data()
-print('Average Watchtime Per Day (Past Year): {0:.2f}'.format(year_avg_watchtime(data)))
-print('Average TV Watchtime Per Day (Past Year): {0:.2f}'.format(year_avg_watchtime(data, True)))
+val_year_avg_watchtime = year_avg_watchtime(data)
+print('Average Watchtime Per Day (Past Year): {0:.2f}'.format(val_year_avg_watchtime))
+val_year_avg_tv_watchtime = year_avg_watchtime(data, True)
+print('Average TV Watchtime Per Day (Past Year): {0:.2f}'.format(val_year_avg_tv_watchtime))
 show_list = top_shows(data, 'watchtime')
 print('Top Shows:\n\t1) {0} at {1:.1f} hours\n\t2) {2} at {3:.1f} hours\n\t3) {4} at {5:.2f} hours'.format(
     show_list[0][0], show_list[0][1]/60, show_list[1][0], show_list[1][1]/60, show_list[2][0], show_list[2][1]/60
 ))
+
+avg_all_years = avg_watchtime_all_years(data)
+
+year_avg_parts = []
+for i in avg_all_years.keys():
+    year_avg_parts += [ ['subheader', [i]], ['big_num', ['Average Watchtime Per Day', avg_all_years[i]['avg_all']]],
+            ['big_num', ['Average TV Watchtime Per Day', avg_all_years[i]['avg_tv']]]]
+
+# Now generate HTML report
+parts = [
+        ['header', ['Trakt.tv Report']],
+        ['top3', ['Top Shows', show_list[0][0], show_list[1][0], show_list[2][0]]]
+        ]
+parts += year_avg_parts
+generator.build_report('trakttv_main', parts)
