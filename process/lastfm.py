e55747101c6ccf313aac7789c77c073a28d78560
Noah Huber-Feely <nhuberfeely@gmail.com>
2019-02-01 20:20:55 -0500

    [lastfm] add top songs and top artists to report

--- process/lastfm.py
+++ process/lastfm.py
@@ -20,6 +20,7 @@ except:
 #songs_recorded = len(json_data['data'])
 songs_recorded = 0
 artist_hits = {}
+song_hits = {}
 for i in json_data['data']:
     songs_recorded += len(i)
     for j in i:
@@ -28,8 +29,21 @@ for i in json_data['data']:
             artist_hits[artist] += 1
         except:
             artist_hits[artist] = 1
+        try:
+            song_hits[(j['name'], artist)] += 1
+        except:
+            song_hits[(j['name'], artist)] = 1
 
-print(artist_hits)
+top_artists = sorted(artist_hits.items(), key=lambda kv: kv[1])
+top_artists.reverse()
+
+top_songs = sorted(song_hits.items(), key=lambda kv: kv[1])
+top_songs.reverse()
+top_songs_html_info = ['Top Songs',
+        '{0} by {1}'.format(top_songs[0][0][0], top_songs[0][0][1]),
+        '{0} by {1}'.format(top_songs[1][0][0], top_songs[1][0][1]),
+        '{0} by {1}'.format(top_songs[2][0][0], top_songs[2][0][1])
+        ]
 
 print("Songs recorded: {0}".format(songs_recorded))
 
@@ -37,6 +51,8 @@ print("Songs recorded: {0}".format(songs_recorded))
 # Now generate HTML report
 parts = [
         ['header', ['Last.fm Report']],
-        ['big_num', ['Songs recorded', songs_recorded]]
+        ['big_num', ['Songs recorded', songs_recorded]],
+        ['top3', ['Top Artists', top_artists[0][0], top_artists[1][0], top_artists[2][0]]],
+        ['top3', top_songs_html_info]
         ]
 generator.build_report('lastfm_main', parts)
