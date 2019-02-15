bd451e7d2fc09167bb3bdc57066ab8def1a30b34
Noah Huber-Feely <nhuberfeely@gmail.com>
2019-02-14 20:13:39 -0500

    [swarm] add swarm sync script

---
+++ sync/getSwarm.py
@@ -0,0 +1,29 @@
+import json, requests
+import sys
+from os import path
+sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
+import utils.loadConfig
+
+from utils import loadConfig
+
+config = loadConfig.getConfig()
+
+client_id = config['keys']['foursquare']['client_id']
+client_secret = config['keys']['foursquare']['client_secret']
+access_code = config['keys']['foursquare']['access_code']
+
+url = 'https://api.foursquare.com/v2/users/self/checkins'
+
+params = dict(
+  client_id=client_id,
+  client_secret=client_secret,
+  oauth_token=access_code,
+  v='20180323',
+  limit=250
+)
+resp = requests.get(url=url, params=params)
+data = json.loads(resp.text)
+
+data_file = open('records/swarm-checkins.json', 'w')
+data_file.write(json.dumps(data))
+data_file.close()
