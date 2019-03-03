0810da43ccff9a77642b4de1194c3aae42ea404e
Noah Huber-Feely <nhuberfeely@gmail.com>
2019-03-02 20:14:45 -0500

    [swarm] add in top places calculation

--- process/swarm.py
+++ process/swarm.py
@@ -17,9 +17,11 @@ except:
     sys.exit()
 
 zipcode_count = {}
+places_count = {}
 
 # Main processing loop
 for i,val in enumerate(json_data['checkins']['items']):
+    #print(val)
     try:
         zipcode_count[val['venue']['location']['postalCode']] += 1
     except:
@@ -27,11 +29,22 @@ for i,val in enumerate(json_data['checkins']['items']):
             zipcode_count[val['venue']['location']['postalCode']] = 1
         except:
             pass # Doesn't have zipcode
+    try:
+        places_count[val['venue']['name']] += 1
+    except:
+        try:
+            places_count[val['venue']['name']] = 1
+        except:
+            pass # Doesn't have name
     pass
 
 top_zipcodes = sorted(zipcode_count.items(), key=lambda kv:kv[1])
 top_zipcodes.reverse()
 topzips_html = generator.build_top3('Top Zipcodes', top_zipcodes, True)
+
+top_places = sorted(places_count.items(), key=lambda kv:kv[1])
+top_places.reverse()
+topplaces_html = generator.build_top3('Top Places', top_places, True)
 
 checkin_total = json_data['checkins']['count']
 print('Total check-ins:', checkin_total)
@@ -39,6 +52,7 @@ print('Total check-ins:', checkin_total)
 parts = [
         ['header', ['Swarm Report']],
         ['big_num', ['Check-in Count', checkin_total]],
-        topzips_html
+        topzips_html,
+        topplaces_html
         ]
 generator.build_report('swarm_main', parts)
