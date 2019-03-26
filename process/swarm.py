c9c902f4db36297545fdfc228fb5360038d9d635
Noah Huber-Feely <nhuberfeely@gmail.com>
2019-03-25 20:48:11 -0400

    [swarm] add sentiment analyzer to swarm processor

--- process/swarm.py
+++ process/swarm.py
@@ -7,6 +7,11 @@ sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
 from utils import loadConfig
 import utils
 import generator
+try:
+    HAS_SENTIMENT = True
+    from utils import sentiment
+except ImportError:
+    HAS_SENTIMENT = False
 
 config = loadConfig.getConfig()
 
@@ -22,9 +27,9 @@ state_count = {}
 
 #print(json_data['checkins']['items'][0])
 
+shout_sentiments = []
 # Main processing loop
 for i,val in enumerate(json_data['checkins']['items']):
-    #print(val)
     try:
         zipcode_count[val['venue']['location']['postalCode']] += 1
     except:
@@ -46,7 +51,19 @@ for i,val in enumerate(json_data['checkins']['items']):
             places_count[val['venue']['name']] = 1
         except:
             pass # Doesn't have name
-    pass
+    if HAS_SENTIMENT:
+        try:
+            post_sentiment = sentiment.assess(val['shout'])
+            shout_sentiments += [post_sentiment['compound']]
+        except KeyError:
+            pass
+
+if HAS_SENTIMENT:
+    avg_sentiment = sum(shout_sentiments)/len(shout_sentiments)
+    sentiment_html = ['big_num', ['Average Shout Sentiment', '{:.1f}% positive'.format(avg_sentiment*100)]]
+else:
+    sentiment_html = ['big_num', ['Average Shout Sentiment', 'Not configured']]
+
 
 def get_top_entry(dictionary, name):
     top = sorted(dictionary.items(), key=lambda kv:kv[1])
@@ -63,6 +80,7 @@ print('Total check-ins:', checkin_total)
 parts = [
         ['header', ['Swarm Report']],
         ['big_num', ['Check-in Count', checkin_total]],
+        sentiment_html,
         topzips_html,
         topstates_html,
         topplaces_html
