0cbc5cd640c3bca5235d7a5be10756655103b34b
Noah Huber-Feely <nhuberfeely@gmail.com>
2019-05-30 16:48:18 -0400

    [collators/hour_blocks] add collator for RescueTime and Gyroscope

--- process/collators/hour_blocks.py
+++ process/collators/hour_blocks.py
@@ -1,6 +1,7 @@
 import sys
 import datetime
 import json
+import csv
 import os
 from os import path
 import numpy as np
@@ -47,4 +48,113 @@ try:
     out_file.close()
 except FileNotFoundError:
     print('Not set up.')
+
+print('RescueTime...')
+def load_rescuetime_data():
+    data_file = utils.load_file('rescuetime-data.csv')
+    data_reader = csv.reader(data_file)
+    keys = ['timestamp', 'name', 'details', 'type', 'subclass', 'val']
+    records = []
+    for row in data_reader:
+        record = { }
+        for i,val in enumerate(keys):
+            if val == 'val':
+                record[val] = int(row[i])
+            else:
+                record[val] = row[i]
+        records += [record]
+    return { 'records': records }
+try:
+    computer_data = load_rescuetime_data()
+    computer_hours = {}
+    for i in computer_data['records']:
+        date = i['timestamp']
+        date_obj = datetime.datetime.strptime(date, "%Y-%m-%d %H:%M:%S %z")
+        date_obj = date_obj.replace(minute=0, second=0, microsecond=0)
+        timestamp = int(date_obj.timestamp())
+        try:
+            computer_hours[timestamp] += [i]
+        except:
+            computer_hours[timestamp] = [i]
+    hour_blocks = []
+    for i in computer_hours.keys():
+        block = {
+                'timestamp': i,
+                'computerScore': 0
+                }
+        try:
+            computer_data = computer_hours[i]
+            for comp in computer_data:
+                block['computerScore'] += comp['val']
+        except KeyError:
+            pass
+        hour_blocks += [block]
+    csv_contents = ''
+    for i in hour_blocks:
+        csv_contents += '{0},{1}\n'.format(i['timestamp'], i['computerScore'])
+    out_file = open('aggregates/hour_blocks_rescuetime_val_sum.csv', 'w')
+    out_file.write(csv_contents)
+    out_file.close()
+except FileNotFoundError:
+    print('Not set up.')
+
+print('Gyroscope...')
+all_export_files = os.listdir('records/gyroscope/')
+# Load all day HR data
+hr_files = []
+for i in all_export_files:
+    beginning_indicator = 'gyroscope-Noah-hr-export'
+    if i[:len(beginning_indicator)] == beginning_indicator:
+        hr_files += [i]
+hr_data = []
+times_added = {}
+for i in hr_files:
+    csv_data = open('records/gyroscope/' + i, 'r')
+    data_reader = csv.reader(csv_data)
+    first_row = True
+    for row in data_reader:
+        if first_row:
+            first_row = False
+            continue
+        try: # Verify this is a new time
+            times_added[row[0]]
+        except KeyError:
+            row_data = {
+                    'time': row[0],
+                    'bpm': int(row[1]),
+                    'service': row[2]
+                    }
+            times_added[row[0]] = True
+            hr_data += [row_data]
+heart_rate_hours = {}
+for i in hr_data:
+    date = i['time']
+    date_obj = datetime.datetime.strptime(date, "%Y-%m-%d-%H:%M:%S")
+    date_obj = date_obj.replace(minute=0, second=0, microsecond=0)
+    timestamp = int(date_obj.timestamp())
+    try:
+        heart_rate_hours[timestamp] += [i]
+    except:
+        heart_rate_hours[timestamp] = [i]
+hour_blocks = []
+for i in heart_rate_hours.keys():
+    block = {
+            'timestamp': i,
+            'avgHr': 0
+            }
+    try:
+        vals = heart_rate_hours[i]
+        for comp in vals:
+            block['avgHr'] += comp['bpm']
+        block['avgHr'] /= len(vals) # Convert to avg
+    except KeyError:
+        pass
+    hour_blocks += [block]
+csv_contents = ''
+for i in hour_blocks:
+    csv_contents += '{0},{1}\n'.format(i['timestamp'], i['avgHr'])
+out_file = open('aggregates/hour_blocks_gyroscope_avg_hr.csv', 'w')
+out_file.write(csv_contents)
+out_file.close()
+
 print('Done.')
