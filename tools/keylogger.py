b5d226da8393230687d2877ec4ab89eb17b10c93
Noah Huber-Feely <nhuberfeely@gmail.com>
2019-09-01 18:04:02 -0500

    [tools] add actual logging to keylogger

--- tools/keylogger.py
+++ tools/keylogger.py
@@ -1,20 +1,27 @@
 # This keylogger attempts to give useful insights into keyboard use
 
 from pynput import keyboard
+import time
+
+log_file = open('records/keylogs.csv', 'a')
 
 def on_press(key):
     try:
         print('alphanumeric key {0} pressed'.format(
             key.char))
+        line = '{0},{1}\n'.format(time.time(), key.char)
     except AttributeError:
         print('special key {0} pressed'.format(
             key))
+        line = '{0},{1}\n'.format(time.time(), key)
+    log_file.write(line)
 
 def on_release(key):
     print('{0} released'.format(
         key))
     if key == keyboard.Key.esc:
         # Stop listener
+        log_file.close()
         return False
 
 # Collect events until released
